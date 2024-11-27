5af3ce58a114b395bb21a773dd80dad240bca5e5
Netra <nahindocha@conestogac.on.ca>
2024-11-26 22:54:42 -0500

    NG-3 Converted js to tsx config and implemented project setup again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,9 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+// @ts-check
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
-    },
-  },
-];
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended
+);
